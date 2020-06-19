3099659569edc68780f9c6547e7d5adf911a79bd
Zachary Wade <zacharywade@gmail.com>
2020-06-19 11:12:52 -0400

    Add support for PIXI 5.3

--- test/__utils__/parseBitmapFont.js
+++ test/__utils__/parseBitmapFont.js
@@ -1,8 +1,13 @@
-import { BitmapFontLoader } from 'pixi.js'
+import { BitmapFont } from 'pixi.js'
 import { emptyTexture } from '../__fixtures__/textures'
 
 const parseBitmapFontData = (data, type = 'text/xml', texture = emptyTexture) => {
-  return BitmapFontLoader.parse({ data: new window.DOMParser().parseFromString(data, type) }, texture)
+  // In JSDOM, XMLDocument is an empty class that extends Document
+  // Since PIXI is looking explicitly for an XMLDocument, we need to either
+  // monkey patch the prototype stack of the resulting document, or just
+  // overwrite XMLDocument with Document
+  global.XMLDocument = Document
+  return BitmapFont.install(new window.DOMParser().parseFromString(data, type), texture)
 }
 
 export default parseBitmapFontData
